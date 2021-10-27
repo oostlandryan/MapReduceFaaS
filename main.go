fb7f7b53b97fd75dfa28250088dba3a159783f81
Ryan Oostland <oostlandryan16@gmail.com>
2021-10-26 21:29:23 -0400

    Add mapCloud and reduceCloud functions

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 )
 
 type mrTuple struct {
@@ -14,12 +15,17 @@ type mrTuple struct {
 }
 
 func main() {
-	f := []string{"testfile"}
-	log.Println(mapCloud(f))
+	f := []string{"PrideandPrejudice"}
+	intermediate := mapCloud(f)
+	result := reduceCloud(intermediate)
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Count < result[j].Count
+	})
+	log.Println(result)
 }
 
 func mapCloud(files []string) []mrTuple {
-	reduceFuncUrl := "https://us-central1-cloud-computing-327315.cloudfunctions.net/MapHttp"
+	mapFuncUrl := "https://us-central1-cloud-computing-327315.cloudfunctions.net/MapHttp"
 
 	j := struct {
 		Files []string `json:"Files"`
@@ -28,7 +34,7 @@ func mapCloud(files []string) []mrTuple {
 	postBody, _ := json.Marshal(j)
 	responseBody := bytes.NewBuffer(postBody)
 
-	resp, err := http.Post(reduceFuncUrl, "application/json", responseBody)
+	resp, err := http.Post(mapFuncUrl, "application/json", responseBody)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -48,6 +54,32 @@ func mapCloud(files []string) []mrTuple {
 	return output.MapResult
 }
 
-func reduceCloud() {
+func reduceCloud(input []mrTuple) []mrTuple {
+	reduceFuncUrl := "https://us-central1-cloud-computing-327315.cloudfunctions.net/ReduceHttp"
 
+	j := struct {
+		MapResult []mrTuple `json:"mapresult"`
+	}{input}
+
+	postBody, _ := json.Marshal(j)
+	responseBody := bytes.NewBuffer(postBody)
+
+	resp, err := http.Post(reduceFuncUrl, "application/json", responseBody)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	output := struct {
+		ReduceResult []mrTuple `json:"reduceresult"`
+	}{}
+	json.Unmarshal(body, &output)
+
+	return output.ReduceResult
 }
